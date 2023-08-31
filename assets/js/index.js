c754c6b21b291992abd2d6ec5d7dfcb66f6f4ac7
natan-matos-souza <natanmatos_18@hotmail.com>
2023-08-30 22:21:28 -0300

    creating stop and resume feature

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,93 +1,90 @@
-function app()
-{
+function app() {
+  const startBtn = document.querySelector("#start-btn");
 
-    const startBtn = document.querySelector("#start-btn");
+  const secondCounter = document.querySelector("#second");
+  const minuteCounter = document.querySelector("#minute");
+  const hourCounter = document.querySelector("#hour");
 
-    const secondCounter = document.querySelector("#second");
-    const minuteCounter = document.querySelector("#minute");
-    const hourCounter = document.querySelector("#hour");
+  const separatorSpan = document.querySelectorAll("#separator");
 
-    const separatorSpan = document.querySelectorAll("#separator");
+  const stopBtn = document.querySelector("#stop-btn");
+  const resumeBtn = document.querySelector("#resume-btn");
 
-    const stopBtn = document.querySelector("#stop-btn");
+  let isCounting = false;
 
-    let isCouting = false;
+  function startCounter() {
+   
+    function countHour() {
+      const getHour = parseInt(hourCounter.innerHTML);
 
+      if (getHour != 99) {
+        hourCounter.innerHTML = getHour + 1;
 
-    function changeTimeHudColor(color)
-    {
-        secondCounter.style.color = color;
-        minuteCounter.style.color = color;
-        hourCounter.style.color = color;
-
-        separatorSpan.forEach((span) => {
-
-            span.style.color = color;
-
-        });
+        minuteCounter.innerHTML = 0;
+        secondCounter.innerHTML = 0;
+      }
     }
 
-    stopBtn.addEventListener('click', () => {
+    function countMinute() {
+      const getMinute = parseInt(minuteCounter.innerHTML);
 
-        clearInterval(secondLoop);
-        isCouting = false;  
+      if (getMinute != 59) {
+        minuteCounter.innerHTML = getMinute + 1;
+        secondCounter.innerHTML = 0;
+      } else {
+        countHour();
+      }
+    }
 
-        changeTimeHudColor('#FF0000');
+    if (isCounting == false) {
+      changeTimeHudColor("#000");
 
+      isCounting = true;
+
+      secondLoop = setInterval(() => {
+        const getSecond = parseInt(secondCounter.innerHTML);
+
+        getSecond == 59
+          ? countMinute()
+          : (secondCounter.innerHTML = getSecond + 1);
+      }, 1 * 1000);
+    }
+  }
+
+  function restartTime() {
+    const valuesToRestart = [secondCounter, minuteCounter, hourCounter];
+
+    valuesToRestart.forEach((counter) => {
+      counter.innerHTML = 0;
     });
+  }
 
-    startBtn.addEventListener('click', () => {
+  function changeTimeHudColor(color) {
+    secondCounter.style.color = color;
+    minuteCounter.style.color = color;
+    hourCounter.style.color = color;
 
-
-        function countHour()
-        {
-            const getHour = parseInt(hourCounter.innerHTML);
-
-            if (getHour != 99)
-            {
-                hourCounter.innerHTML = getHour + 1;
-
-                minuteCounter.innerHTML = 0;
-                secondCounter.innerHTML = 0;
-
-            }
-        }
-
-        function countMinute()
-        {
-            const getMinute = parseInt(minuteCounter.innerHTML);
-
-            if (getMinute != 59)
-            {
-                minuteCounter.innerHTML = getMinute + 1;
-                secondCounter.innerHTML = 0;
-            } else {
-
-                countHour();
-
-            }
-
-        }
-
-        if (isCouting == false)
-        {
-
-            changeTimeHudColor('#000');
-            
-            isCouting = true;
-
-            secondLoop = setInterval(() => {
-            
-                const getSecond = parseInt(secondCounter.innerHTML);
-                
-                (getSecond == 59) ? countMinute() : secondCounter.innerHTML = getSecond + 1;
-    
-    
-            }, 1 * 1000);
-        }
-
+    separatorSpan.forEach((span) => {
+      span.style.color = color;
     });
+  }
 
+  stopBtn.addEventListener("click", () => {
+    clearInterval(secondLoop);
+    isCounting = false;
+
+    changeTimeHudColor("#FF0000");
+  });
+
+  startBtn.addEventListener("click", () => {
+    restartTime();
+    startCounter();
+  });
+
+  resumeBtn.addEventListener("click", () => {
+    changeTimeHudColor("#000");
+    startCounter();
+  });
 }
 
 app();
