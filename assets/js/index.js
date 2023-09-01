22156d1a63964e664e9db3df0ade170b42c109c9
natan-matos-souza <natanmatos_18@hotmail.com>
2023-09-01 12:17:19 -0300

    first commit

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,90 +1,48 @@
-function app() {
-  const startBtn = document.querySelector("#start-btn");
+function app()
+{
+    const submitBtn = document.querySelector('#submit-btn');
+    const toDoListArea = document.querySelector('#to-do-area');
 
-  const secondCounter = document.querySelector("#second");
-  const minuteCounter = document.querySelector("#minute");
-  const hourCounter = document.querySelector("#hour");
+    function getInputData()
+    {
+        const mainInput = document.querySelector('#input');
+        const inputValue = mainInput.value;
 
-  const separatorSpan = document.querySelectorAll("#separator");
+        mainInput.value = "";
 
-  const stopBtn = document.querySelector("#stop-btn");
-  const resumeBtn = document.querySelector("#resume-btn");
-
-  let isCounting = false;
-
-  function startCounter() {
-   
-    function countHour() {
-      const getHour = parseInt(hourCounter.innerHTML);
-
-      if (getHour != 99) {
-        hourCounter.innerHTML = getHour + 1;
-
-        minuteCounter.innerHTML = 0;
-        secondCounter.innerHTML = 0;
-      }
+        return inputValue;
     }
 
-    function countMinute() {
-      const getMinute = parseInt(minuteCounter.innerHTML);
 
-      if (getMinute != 59) {
-        minuteCounter.innerHTML = getMinute + 1;
-        secondCounter.innerHTML = 0;
-      } else {
-        countHour();
-      }
-    }
+    submitBtn.addEventListener('click', (e) => {
 
-    if (isCounting == false) {
-      changeTimeHudColor("#000");
+        e.preventDefault();
 
-      isCounting = true;
+        const data = getInputData();
 
-      secondLoop = setInterval(() => {
-        const getSecond = parseInt(secondCounter.innerHTML);
+        toDoListArea.innerHTML += `<li>
+        <span>${data}</span>
+        <input type="button" id="remove-btn" value="Apagar">
+        </li>`;
 
-        getSecond == 59
-          ? countMinute()
-          : (secondCounter.innerHTML = getSecond + 1);
-      }, 1 * 1000);
-    }
-  }
-
-  function restartTime() {
-    const valuesToRestart = [secondCounter, minuteCounter, hourCounter];
-
-    valuesToRestart.forEach((counter) => {
-      counter.innerHTML = 0;
     });
-  }
 
-  function changeTimeHudColor(color) {
-    secondCounter.style.color = color;
-    minuteCounter.style.color = color;
-    hourCounter.style.color = color;
 
-    separatorSpan.forEach((span) => {
-      span.style.color = color;
+    //Removendo dados
+    document.addEventListener('click', (e) => {
+        
+        const target = e.target;
+
+        switch (target.id)
+        {
+            case 'remove-btn':
+                const targetParentElement = target.parentElement;
+                targetParentElement.remove();
+                break;
+        }
+
     });
-  }
 
-  stopBtn.addEventListener("click", () => {
-    clearInterval(secondLoop);
-    isCounting = false;
-
-    changeTimeHudColor("#FF0000");
-  });
-
-  startBtn.addEventListener("click", () => {
-    restartTime();
-    startCounter();
-  });
-
-  resumeBtn.addEventListener("click", () => {
-    changeTimeHudColor("#000");
-    startCounter();
-  });
 }
 
 app();
